5c72a74ad1a32984c5c72d482d2560cbd26e2ee4
T1M1D0 <arena552@proton.me>
2025-05-05 21:56:08 -0400

    Add files via upload

---
+++ quizlet.py
@@ -0,0 +1,18 @@
+import re
+
+
+def main():
+    output = 'quizlet.txt'
+    words_list = input("Type the words you want to split into a text file to convert to duckyscript:\n")
+    word = re.split('[:-]', words_list)
+    print(word)
+    with open(output, 'w', encoding='utf-8') as f:
+        f.write('DEFAULTDELAY 1000\n')
+        for p in range(len(word)):
+            f.write('STRING ' + word[p] + '\n')
+            f.write('TAB\n')
+            p+=1
+
+
+if __name__ == "__main__":
+    main()
