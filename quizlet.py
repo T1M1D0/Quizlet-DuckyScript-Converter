a49409f770c285acc15cad2939fdd440c5b3f816
T1M1D0 <arena552@proton.me>
2025-05-07 22:50:58 -0400

    Update quizlet.py

--- quizlet.py
+++ quizlet.py
@@ -1,17 +1,14 @@
 import re
-import subprocess
 
 #Functions are sloppy they are just made to make it easier to read
 #Not a great Programmer, very sloppy code
 
 def rw():
     with open('convert.txt', 'r') as convert:
-        words = convert.read()
-    subprocess.run(['/bin/bash', '/home/t1m1d/Code/Python/AutoQuiz/script.sh'])
-    with open('converted.txt', 'r') as converted:
-        words_list = converted.read()
-    words_lists = re.split('[:;]', words_list)
-    ducky_conv(words_lists)
+        word_list_raw = convert.read()
+    word_list_raw = re.split('[:\n]', word_list_raw)
+    word_list = [key.strip() for key in word_list_raw]
+    ducky_conv(word_list)
 
 def ducky_conv(wL):
     with open('quizlet.txt', 'w', encoding='utf-8') as f:
@@ -23,7 +20,8 @@ def ducky_conv(wL):
 
 def main():
     output = 'quizlet.txt'
-    print("Upload a file called 'convert.txt' and populate it with what words you want to convert\n")
+    print("Upload a file called 'convert.txt' and populate it with what words you want to convert")
+    print("If this is done, enjoy your new script to finish your quizlet which will be found in the 'quizlet.txt file'")
     rw()
 
 if __name__ == "__main__":
